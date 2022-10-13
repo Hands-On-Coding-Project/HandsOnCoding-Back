f6bd3fa061335662912a5933fe291fe5af129dbc
Jaime Chavarriaga <JaimeChavarriaga@users.noreply.github.com>
2022-10-13 01:45:48 +0000

    Agrega scripts para ejecutar docker en Windows

---
+++ create-databases.cmd
@@ -0,0 +1,8 @@
+echo "Crea bases de datos"
+docker exec -it mongo ^
+    mongosh -u root -p example ^
+    --eval "db = db.getSiblingDB('dev'); db.createCollection('dummy');"
+
+docker exec -it mongo ^
+    mongosh -u root -p example ^
+    --eval "db = db.getSiblingDB('test'); db.createCollection('dummy');"
---
+++ delete-containers.cmd
@@ -0,0 +1,3 @@
+echo "Detiene servicios en ejecución"
+docker rm -f mongo
+docker rm -f mongo-express
